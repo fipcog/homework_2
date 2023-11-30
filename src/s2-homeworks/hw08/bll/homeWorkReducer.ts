c2ef367d72fed124550c13bf8898a183ac0acbc5
Artur <looppinooppi@gmail.com>
2023-11-30 15:35:47 +0300

    done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { 
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': { 
+            let newState = [...state]
+            if(action.payload === 'up') newState.sort((a, b) => a.name > b.name ? 1 : -1)
+            if(action.payload === 'down') newState.sort((a, b) => a.name < b.name ? 1 : -1)
+            return newState
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= 18)
         }
         default:
             return state
     }
 }
+
+
